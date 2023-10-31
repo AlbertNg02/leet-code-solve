64127ad03e9b72615d284a4db306e846598fa27c
Albert_Nguyen <77342590+AlbertNg02@users.noreply.github.com>
2023-10-31 14:28:23 -0500

    2 leetcode 2,9

---
+++ 9.palindrome-number.py
@@ -0,0 +1,77 @@
+#
+# @lc app=leetcode id=9 lang=python3
+#
+# [9] Palindrome Number
+#
+# https://leetcode.com/problems/palindrome-number/description/
+#
+# algorithms
+# Easy (53.91%)
+# Likes:    10352
+# Dislikes: 2552
+# Total Accepted:    3.4M
+# Total Submissions: 6.3M
+# Testcase Example:  '121'
+#
+# Given an integer x, return true if x is a palindrome, and false otherwise.
+# 
+# 
+# Example 1:
+# 
+# 
+# Input: x = 121
+# Output: true
+# Explanation: 121 reads as 121 from left to right and from right to left.
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: x = -121
+# Output: false
+# Explanation: From left to right, it reads -121. From right to left, it
+# becomes 121-. Therefore it is not a palindrome.
+# 
+# 
+# Example 3:
+# 
+# 
+# Input: x = 10
+# Output: false
+# Explanation: Reads 01 from right to left. Therefore it is not a
+# palindrome.
+# 
+# 
+# 
+# Constraints:
+# 
+# 
+# -2^31 <= x <= 2^31 - 1
+# 
+# 
+# 
+# Follow up: Could you solve it without converting the integer to a string?
+#
+
+# @lc code=start
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x<0: return False
+
+        div = 1
+        while x>=10 * div:
+            div *= 10
+        while x:
+            right = x % 10
+            left = x // div
+
+            if left != right: return False
+
+            x = (x % div ) //10
+            div = div / 100
+        return True
+
+
+        
+# @lc code=end
+
